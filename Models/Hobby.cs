9010ef9a9eec4e4dd1cbf6d9d0c16957d4307cb9
Perez55 <93294139+Perez55@users.noreply.github.com>
2022-11-01 14:55:43 -0400

    Final Project Setup

---
+++ Models/Hobby.cs
@@ -0,0 +1,12 @@
+namespace Final_Project.Models;
+public class Hobby 
+{
+    //This model will be about basic information about the hobby; this will be the table in the datbase
+    public string Name {get;set;}
+    public string Desc {get;set;}
+
+
+
+
+
+}
