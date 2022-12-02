76ecede596a32aec2d3aef23504ba87cbd6c164f
Perez55 <93294139+Perez55@users.noreply.github.com>
2022-12-02 15:37:31 -0500

    Merge branch 'master' into Final-Project_V2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,101 +1,103 @@
-﻿using System;
-using System.Linq;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Final_Project.Models;
+using System;
 using Microsoft.EntityFrameworkCore;
-using ReadingList.Models;
 
-namespace ReadingList.Controllers
+
+namespace Final_Project.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private BookContext context;
+    public HomeController(BookContext ctx) => context = ctx;
+
+    public IActionResult Index(string id)
     {
-        private BookContext context;
-        public HomeController(BookContext ctx) => context = ctx;
+        // load current filters and data needed for filter drop downs in ViewBag
+        var filters = new Filters(id);
+        BookViewModel model = new BookViewModel();
+        model.Filters = filters;
+        model.Genres = context.Genres.ToList();
+        model.ReadFilters = Filters.ReadFilterValues;
 
-        public IActionResult Index(string id)
+        // get Book objects from database based on current filters
+        IQueryable<Book> query = context.Books
+            .Include(t => t.Genre);
+        if (filters.HasGenre)
         {
-            // load current filters and data needed for filter drop downs in ViewBag
-            var filters = new Filters(id);
-            BookViewModel model = new BookViewModel();
-            model.Filters = filters;
-            model.Genres = context.Genres.ToList();
-            model.ReadFilters = Filters.ReadFilterValues;
-
-            // get Book objects from database based on current filters
-            IQueryable<Book> query = context.Books
-                .Include(t => t.Genre);
-            if (filters.HasGenre) {
-                query = query.Where(t => t.GenreId == filters.GenreId);
-            }
-            if (filters.HasDue) {
-                var today = DateTime.Today;
-                if (filters.IsPast)
-                    query = query.Where(t => t.CompleteDate < today);
-                else if (filters.IsFuture)
-                    query = query.Where(t => t.CompleteDate > today);
-                else if (filters.IsToday)
-                    query = query.Where(t => t.CompleteDate == today);
-            }
-            model.Books = query.OrderBy(t => t.CompleteDate).ToList();
-            return View(model);
+            query = query.Where(t => t.GenreId == filters.GenreId);
         }
-
-        public IActionResult Add()
+        if (filters.HasDue)
         {
-            BookViewModel model = new BookViewModel();
-            model.Genres = context.Genres.ToList();
-            return View(model);
+            var today = DateTime.Today;
+            if (filters.IsPast)
+                query = query.Where(t => t.CompleteDate < today);
+            else if (filters.IsFuture)
+                query = query.Where(t => t.CompleteDate > today);
+            else if (filters.IsToday)
+                query = query.Where(t => t.CompleteDate == today);
         }
+        model.Books = query.OrderBy(t => t.CompleteDate).ToList();
+        return View(model);
+    }
 
-        public IActionResult About()
-        {
-            return View();
-        }
+    public IActionResult Add()
+    {
+        BookViewModel model = new BookViewModel();
+        model.Genres = context.Genres.ToList();
+        return View(model);
+    }
 
-        [HttpPost]
-        public IActionResult Add(BookViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                context.Books.Add(model.CurrentBook);
-                context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                model.Genres = context.Genres.ToList();
-                return View(model);
-            }
-        }
+    public IActionResult About()
+    {
+        return View();
+    }
 
-        [HttpPost]
-        public IActionResult Filter(string[] filter)
+    [HttpPost]
+    public IActionResult Add(BookViewModel model)
+    {
+        if (ModelState.IsValid)
         {
-            string id = string.Join('-', filter);
-            return RedirectToAction("Index", new { ID = id });
-        }
-
-        [HttpPost]
-        public IActionResult Edit([FromRoute]string id, string action, Book selected)
-        {
-            if (action == "delete")
-            {
-                context.Books.Remove(selected);
-            }
-            else if (action == "start")
-            {
-                selected = context.Books.Find(selected.Id);
-                selected.StartDate = DateTime.Today;
-                context.Books.Update(selected);
-            }
-            else if (action == "complete")
-            {
-                selected = context.Books.Find(selected.Id);
-                selected.CompleteDate = DateTime.Today;
-                context.Books.Update(selected);
-            }
+            context.Books.Add(model.CurrentBook);
             context.SaveChanges();
-
-            return RedirectToAction("Index", new { ID = id });
+            return RedirectToAction("Index");
         }
+        else
+        {
+            model.Genres = context.Genres.ToList();
+            return View(model);
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Filter(string[] filter)
+    {
+        string id = string.Join('-', filter);
+        return RedirectToAction("Index", new { ID = id });
+    }
+
+    [HttpPost]
+    public IActionResult Edit([FromRoute] string id, string action, Book selected)
+    {
+        if (action == "delete")
+        {
+            context.Books.Remove(selected);
+        }
+        else if (action == "start")
+        {
+            selected = context.Books.Find(selected.Id);
+            selected.StartDate = DateTime.Today;
+            context.Books.Update(selected);
+        }
+        else if (action == "complete")
+        {
+            selected = context.Books.Find(selected.Id);
+            selected.CompleteDate = DateTime.Today;
+            context.Books.Update(selected);
+        }
+        context.SaveChanges();
+
+        return RedirectToAction("Index", new { ID = id });
     }
 }
